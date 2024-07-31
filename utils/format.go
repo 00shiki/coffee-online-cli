e072d6bddf7e8f4df4f868fa751fcfd4f4fba3ef
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2024-07-31 19:55:59 +0700

    fix price formatting

--- utils/format.go
+++ utils/format.go
@@ -1,11 +1,15 @@
 package utils
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func PriceFormat(price float64) string {
 	s := strconv.FormatFloat(price, 'f', 2, 64)
+	s = strings.Replace(s, ".", ",", 1)
 	for i := len(s) - 6; i > 0; i -= 3 {
-		s = s[:i] + "," + s[i:]
+		s = s[:i] + "." + s[i:]
 	}
 	return s
 }
